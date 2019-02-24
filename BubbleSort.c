117c33561cac8f98383b6a319fb704f9d1d3fad0
SANTANIL BASAK <santanilbasak@gmail.com>
2019-02-24 15:45:45 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,23 +1,21 @@
 #include<stdio.h>
 
-void bubbleSort(int *arr,int n)
+void bubbleSort(int arr[],int n)
 {
 	int i,j,temp;
-	for(i=0;i<(n-1);i++)
+	for(i=1;i<=(n-1);i++)
 	{
-		for(j=0;j<(n-i-1);j++)
+		for(j=0;j<=(n-i-1);j++)
 		{
-			if(*(arr+j)>*(arr+(j+1)) )
+			if(arr[j]>arr[j+1])
 			{
-				temp=*(arr+j);
-				*(arr+j)=*(arr+(j+1));
-				*(arr+(j+1))=temp;	
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;	
 			}	
 		}	
 	}	
 }
-
-
 void main()
 {
 	int arr[100];
@@ -33,5 +31,5 @@ void main()
 	printf("Sorted Array: \n");
 	
 	for(i=0;i<n;i++)
-		printf("%d  ",*(arr+i));
+		printf("%d  ",arr[i]);
 }
